aee0589580ab354f8af11ca35cc5a5e98ce56fe4
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-17 20:50:02 +0900

    feat(Sidebar): implement Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,24 @@ import { FC } from "react";
 import "./Sidebar.css";
 
 const Sidebar: FC = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>MD Notes</h1>
+        <button>New Note</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>Title</strong>
+            <button>Delete</button>
+          </div>
+          <p>Note Contents</p>
+          <small>Last update: xx/xx</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
